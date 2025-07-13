a494f8cf23edf49f17e36c8468deab3433cc1ff0
pattala <137921767+pattala@users.noreply.github.com>
2025-07-13 18:36:42 -0300

    Update send-notification.js

--- api/send-notification.js
+++ api/send-notification.js
@@ -43,7 +43,7 @@ module.exports = async (req, res) => {
                 data: {
                     title: title,
                     body: body,
-                    icon: 'https://i.postimg.cc/tJgqS2sW/mi-logo.png' // URL del logo
+                    icon: 'https://github.com/pattala/rampet-cliente-app/blob/main/images/mi_logo.png' // URL del logo
                 }
             },
         };
