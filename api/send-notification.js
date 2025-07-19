ce7474e660f6155019b4a9d29c13b3a48915e0fd
pattala <137921767+pattala@users.noreply.github.com>
2025-07-19 03:40:08 -0300

    Update send-notification.js

--- api/send-notification.js
+++ api/send-notification.js
@@ -1,4 +1,4 @@
-//A
+//noti
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const { getMessaging } = require('firebase-admin/messaging');
