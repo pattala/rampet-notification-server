4284c662a4fe15127b2e13c40e44c1967da2ec04
pattala <137921767+pattala@users.noreply.github.com>
2025-08-14 11:56:06 -0300

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -1,3 +1,4 @@
+import { resolveTemplate, applyBlocksAndVars, sanitizePush } from '../utils/templates.js';
 import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
